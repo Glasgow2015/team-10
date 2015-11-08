76184c8379972949cd7ffa3554ec1238d99f00c8
evusas <evusas@gmail.com>
2015-11-08 08:11:30 +0000

    Quiz page

--- quiz.js
+++ quiz.js
@@ -1,31 +1,45 @@
 $(document).ready(function() {
 
-		var map = new Map();
+		var toggler = 0;
+
+/*		var map = new Map();
+		map.put( $(this).id , 0 );	
+		* 
+		* 
+		* 
+		* 
+		//var key = $(event.toElement).id;	  
+		//!map.value(key);
+		//map.put( itsId, 1);
+		//map.put(itsId, 0);
+		//To be implemented
+*/
+//Map to be implemented 	
+
 	
 		$( ".inline" ).each( 
-		function(){
-		map.put( $(this).id , 0 );
-			
 			function () {
 				$(this).click(fun);
 		});
-					    
+			
+		$("#feedback").click(show);
 
 		function fun(event) {
-
-			  
-			  	var itsId = $(event.toElement).id;	  
-			  
-			  if (  !map.value( itsId)  )  {
+	
+			  if (  !toggler  )  {
 				 $(event.toElement).fadeTo("fast", 1.0);
-				 map.put( itsId, 1);
+				 toggler = 1;
 			 }
 			 
 			 else 			    {
 				 $(event.toElement).fadeTo("fast", 0.3);
-				 map.put(itsId, 0);
+				 toggler = 0;
 			 }
 			 
+		};
+		
+		function show(event) {
+			alert("You might be at risk! You should see your GP.");
 		};
 		
 
