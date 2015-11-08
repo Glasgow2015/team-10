42a4683d18943a6847283b9cbead048fa5810b24
evusas <evusas@gmail.com>
2015-11-08 05:12:41 +0000

    Test page working beta

--- quiz.js
+++ quiz.js
@@ -1,18 +1,40 @@
 $(document).ready(function() {
 
-	//function() { console.log("LALLALA"); };
-	
-    //document.getElementById(boxNo).innerHTML = "Hello World";
-    var diseases 	= new Array();
-	var i = 0;
-	for (i;i<10;i++) {
-		diseases[i]=0;
-	}
-		$( ".inline" ).each(function( index ) {
-		  //console.log( index + ": " + $( this ).text() );
-		  (this).innerHTML = index;
-		   diseases[i] = $(this);
-		   i++;
+		var array = new Array();
+		
+		var i = 0;
+		for (i; i<10; i++) {
+			array[i] = 0; 
+		}	
+
+		//console.log(id + " ");
+		
+		
+		$( ".inline" ).each( function () {
+
+		$(this).click( function() {
+			var idNo = (this).id;
+				    
+
+			 if (!array[idNo])  {
+				 $(this).fadeTo("fast", 1.0);
+				 array[idNo] = 1;
+			 }
+			 
+			 else 			    {
+				 $(this).fadeTo("fast", 0.3);
+				 array[idNo] = 0;
+			 }
+			 
+			 
+			
 		});
-    
+		
+		});
+					    
+			    
+
+		
+		
+
 });
